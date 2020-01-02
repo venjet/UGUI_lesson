6146eb9283bc695ca55decf2f452b534a545348c
venjet <venjet@gmail.com>
2020-01-02 11:50:06 +0800

    prj upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   public float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up*Time.deltaTime*speed);
+    }
+
+    public void ChangeSpeed(float newSpeed){
+        this.speed = newSpeed;
+    }
+}
